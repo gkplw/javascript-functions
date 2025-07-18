5975e4f6a0e4dfed87ba6723fe8d7a9ee637cb60
Pharnuwat <se56160411@gmail.com>
2025-07-18 13:35:29 +0700

    feat: implement basic arithmetic functions and calculator object

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,12 @@
 // Start coding here
-let calculator = {};
+const add = (a,b) => a+b;
+const substract = (a,b) => a-b;
+const multiply = (a,b) => a*b;
+const divide = (a,b) => a/b;
+
+let calculator = {add,substract,multiply,divide};
+
+const addResult = calculator.add(10,20);
+console.log(addResult);
+const divideResult = calculator.divide(3000,10);
+console.log(divideResult);
